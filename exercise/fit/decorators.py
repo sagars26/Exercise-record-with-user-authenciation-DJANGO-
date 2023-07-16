00645a8799e07876ab9cc42b80446aa1d2ac7f8b
sagars26 <sagar260303@gmail.com>
2023-07-16 21:37:22 +0530

    identify users
    function to identify authorised user

--- exercise/fit/decorators.py
+++ exercise/fit/decorators.py
@@ -10,3 +10,19 @@ def unauthorized_user(view_func):
         else:
             return view_func(request,*args,**kwargs)
     return wrapper_func
+
+
+#this functoin will identify alllowed users
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func): 
+        def wrapper_func(request,*args,**kwargs):
+            group=None
+            if request.user.groups.exists():
+                group=request.user.groups.all()[0].name
+            if group in allowed_roles:
+                return view_func(request,*args,**kwargs)
+            else:
+                return HttpResponse("<h1>You are not allowed to access this page</h1>")
+        return wrapper_func
+    return decorator
