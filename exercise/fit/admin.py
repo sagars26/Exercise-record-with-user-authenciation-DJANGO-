191f5fe93e0a7e302b2be19d1a511c6c2825ec9f
sagars26 <sagar260303@gmail.com>
2023-07-16 22:04:12 +0530

    update admin site
    it registers all oir models to admin site

--- exercise/fit/admin.py
+++ exercise/fit/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Now, Register the models here.
+
+class exerAdmin(admin.ModelAdmin):
+    class Meta:
+        model=Exerc
+    list_display=['name']
+    list_filter=['name']
+
+admin.site.register(Customer)
+admin.site.register(Userexer)
+admin.site.register(Category)
+admin.site.register(Exerc,exerAdmin)
